15ea47703f616373a73ae11b588e0ceb569aa84d
ASP <dragon.ka@wp.pl>
2018-02-05 21:40:02 +0100

    mLab

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://<ASP>:<ASP>@ds125318.mlab.com:25318/database-1', {
     useMongoClient: true
 });
 
